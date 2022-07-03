a7c465144dc05660e92c724f00dc9866aab303bb
Ibrahim Sulemana <ibeevpersie@gmail.com>
2022-07-03 20:17:55 +0800

    Updated Markdown

--- README.md
+++ README.md
@@ -1,16 +1,19 @@
 # Python-Scripts
 ## Mini projects from school, Tkinter interface, it was awesome and now Django from Personal Studies
 
-1. Simple Calculator
-2. Test to help a friend print output
-3. Signup  project for a friend
-4. Notepad project
-5. Hex color picker
-6. Time & Date
-7. Stopwatch
-8. Password Generator
-9. Weather App
-10. Register App
-11. Chatt App
-12. Word Counter
-13.Blog
+* Simple Calculator
+* Test to help a friend print output
+* Signup  project for a friend
+* Notepad project
+* Hex color picker
+* Time & Date
+* Stopwatch
+* Password Generator
+* Weather App
+* Register App
+* Chatt App
+* Word Counter
+* Blog
+
+
+
--- django/pollsite/polls/views.py
+++ django/pollsite/polls/views.py
@@ -1,6 +1,27 @@
-from django.shortcuts import render
+from re import template
+from django.http import HttpResponse, Http404
+from django.shortcuts import render, get_object_or_404
+from django.template import loader
+from polls.models import Question
 
 # Create your views here.
 
 def index(request):
-    pass
+    latest_question = Question.objects.order_by('-pub_date')[:5]
+    template = loader.get_template('polls/index.html')
+    context = {
+        'latest_question': latest_question,
+    }
+    return HttpResponse(template.render(context, request))
+
+def detail(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/detail.html', { 'question': question })
+
+def results(request, question_id):
+    response = 'You are looking at the results of question %s.'
+    return HttpResponse(response % question_id)
+
+def votes(request, question_id):
+    return HttpResponse('You are voting on question %s.' % question_id)
+    
